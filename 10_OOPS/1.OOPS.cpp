66574a2e4809945bdd06a91a2d5a74a543b7611c
Muhammad <alaminmir08@gmail.com>
2020-12-30 15:52:31 +0600

    Start Learning OOP

---
+++ 10_OOPS/1.OOPS.cpp
@@ -0,0 +1,42 @@
+/*  What is OOP?
+    OOP stands for Object-Oriented Programming.
+
+    Procedural programming is about writing procedures or functions that perform operations on the data, 
+    while object-oriented programming is about creating objects that contain both data and functions.
+
+    Object-oriented programming has several advantages over procedural programming:
+
+    OOP is faster and easier to execute
+    OOP provides a clear structure for the programs
+    OOP helps to keep the C++ code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
+    OOP makes it possible to create full reusable applications with less code and shorter development time .
+    OOPs makes development and maintenance easier where as in Procedure-oriented programming language it is not easy to manage if code grows as project size grows.
+    OOPs provide data hiding whereas in Procedure-oriented programming language a global data can be accessed from anywhere.
+    OOPs provide ability to simulate real-world event much more effectively. We can provide the solution of real word problem if we are using the Object-Oriented Programming language.
+
+    ** OOP main pillars are: (Inheritance, Polymorphism, Abstraction, Encapsulation)
+
+    **Concepts Provide by OOP: (Object, Class, Inheritance, Polymorphism, Abstraction, Encapsulation);
+
+    Object:
+    Any entity that has state and behavior is known as an object. For example: chair, pen, table, keyboard, bike etc. It can be physical and logical.
+
+    Class:
+    Collection of objects is called class. It is a logical entity.
+
+    Inheritance:
+    When one object acquires all the properties and behaviors of parent object i.e. known as inheritance. It provides code reusability. It is used to achieve runtime polymorphism.
+
+    Polymorphism:
+    When one task is performed by different ways i.e. known as polymorphism. For example: to convince the customer differently, to draw something e.g. shape or rectangle etc.
+
+    In C++, we use Function overloading and Function overriding to achieve polymorphism.
+
+    Abstraction:
+    Hiding internal details and showing functionality is known as abstraction. For example: phone call, we don't know the internal processing.
+
+    In C++, we use abstract class and interface to achieve abstraction.
+
+    Encapsulation:
+    Binding (or wrapping) code and data together into a single unit is known as encapsulation. For example: capsule, it is wrapped with different medicines.
+*/
---
+++ 10_OOPS/2.POP_VS_OOP.cpp
@@ -0,0 +1,13 @@
+/* 
+    POP or Procedure Oriented Programming:
+    * Consits of writhing a set of instructions for the computer to follow.
+    * Main focus is on functions and not on flow of data 
+    * Functions can either use local or global data 
+    * Data moves openly from function to function.
+
+    OOP or Object Oriented Programming
+    * Works on the concept of classes and objects
+    * A class is a template to create objects
+    * Treats data as a critical element 
+    * Decomposes the problem in objects and builds data and functions around the object.
+ */

ea7734d6706dafb9b550745128575c2e8b343fcf
Muhammad <alaminmir08@gmail.com>
2021-01-19 08:15:23 +0600

    change file name and start learning Template in cpp

---
+++ 12_Template/1.Template.cpp
@@ -0,0 +1,14 @@
+/* 
+    A template is a simple and yet very powerful tool in C++. 
+    The simple idea is to pass data type as a parameter so that we don’t need to write the same code for different data types.
+    For example, a software company may need sort() for different data types. Rather than writing and maintaining the multiple codes, we can write one sort() and pass data type as a parameter.
+
+    C++ adds two new keywords to support templates: ‘template’ and ‘typename’. The second keyword can always be replaced by keyword ‘class’.
+    Templates are powerful features of C++ which allows you to write generic programs. In simple terms, you can create a single function or a class to work with different data types using templates.
+
+    Templates are often used in larger codebase for the purpose of code reusability and flexibility of the programs.
+
+    The concept of templates can be used in two different ways:
+    * Function Templates
+    * Class Templates
+ */

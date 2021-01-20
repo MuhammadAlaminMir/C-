dacb4d71e1cefd9deb7e6f6793ce41308b750dbe
Muhammad <alaminmir08@gmail.com>
2021-01-20 23:50:08 +0600

    Create a new folder and try to learn stl in c++

---
+++ 12_Template/2.STL/1.STL.cpp
@@ -0,0 +1,16 @@
+/* 
+The C++ STL (Standard Template Library) is a powerful set of C++ template classes to provide general-purpose classes and functions with
+templates that implement many popular and commonly used algorithms and data structures like vectors, lists, queues, and stacks.
+
+At the core of the C++ Standard Template Library are following three well-structured components −
+
+          >>>   Component & Description
+1	Containers
+Containers are used to manage collections of objects of a certain kind. There are several different types of containers like deque, list, vector, map etc.
+
+2	Algorithms
+Algorithms act on containers. They provide the means by which you will perform initialization, sorting, searching, and transforming of the contents of containers.
+
+3	Iterators
+Iterators are used to step through the elements of collections of objects. These collections may be containers or subsets of containers.
+ */
---
+++ 12_Template/2.STL/2.Containers.cpp
@@ -0,0 +1,16 @@
+/* 
+    A container is a holder object that stores a collection of other objects (its elements). 
+    They are implemented as class templates, which allows a great flexibility in the types supported as elements.
+
+    The container manages the storage space for its elements and provides member functions to access them, 
+    either directly or through iterators (reference objects with similar properties to pointers).
+
+    We have 3 types of containers:
+    1. Sequence containers
+        Used to implement data structures that are sequential in nature like arrays(array) and linked list(list).
+    2. Associative containers
+        Associative containers implement sorted data structures that can be quickly searched (O(log n) complexity).
+    3. Derived Containers
+    
+
+ */
